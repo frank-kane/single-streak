6f3a5798cd3f28ac1deafa0ed8e43cd68ea12692
Kandy-Kane <frankmyster@gmail.com>
2023-09-20 23:30:37 -0400

    Got it working completely and formated the dates and database dates

---
+++ app/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwBJCF6qjyV1Z28vaNmntxAimZdXe_dQY",
+  authDomain: "single-streak.firebaseapp.com",
+  projectId: "single-streak",
+  storageBucket: "single-streak.appspot.com",
+  messagingSenderId: "471226813776",
+  appId: "1:471226813776:web:cb76249c508c43cbdb7055",
+  measurementId: "G-EXFMWLWCS7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+export const db = getFirestore(app);
--- app/page.js
+++ app/page.js
@@ -1,95 +1,160 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
+import {db} from './firebase-config'
+import { useEffect, useState } from 'react'
+import { Timestamp, doc, getDoc, updateDoc  } from "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 export default function Home() {
+  const [streak, setStreak] = useState({}) 
+
+
+
+  useEffect(()  =>{
+    const today = new Date().toLocaleDateString("en-US");
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate()-1)
+    yesterday = yesterday.toLocaleDateString("en-US")
+    
+    async function fetchData() {
+    const docRef = doc(db, "my-info", "1R01JaSkN66l356PKmnM");
+    const docSnap = await getDoc(docRef);
+    const docData = docSnap.data()
+    const startDate = docData.start_date.toDate();
+    const lastCompletedDay = docData.last_completed_day.toDate().toLocaleDateString("en-US");
+
+    
+    
+    console.log("Todays Date: "+today)
+    console.log("Yesterdays Date: "+yesterday)
+    console.log("Start: "+startDate)
+    console.log("Last Completed: "+lastCompletedDay)
+    
+    console.log("streak: "+streak)
+    console.log("last COmpleted Day: "+lastCompletedDay)
+    
+
+    //streak was true for 
+    if(lastCompletedDay == today){
+      console.log('Outline 1')
+      setStreak({
+        current_day: today,
+        is_completed: true,
+        name: docData.name,
+        start_date: docData.start_date,
+        last_completed_day: lastCompletedDay,
+        streak : docData.streak
+      });
+
+    }
+    else if(lastCompletedDay == yesterday){
+      console.log('Outline 2')
+      setStreak({
+        current_day: today,
+        is_completed: false,
+        name: docData.name,
+        start_date: docData.start_date,
+        last_completed_day: lastCompletedDay,
+        streak : docData.streak
+      });
+
+    }else{
+      console.log('Outline 3')
+      setStreak({
+        current_day: today,
+        is_completed: false,
+        name: docData.name,
+        start_date: docData.start_date,
+        last_completed_day: lastCompletedDay,
+        streak : 0
+      });
+
+    }
+
+    // if (docSnap.exists()) {
+    //   console.log("Document data:", docData);
+
+    // } else {
+    //   // docSnap.data() will be undefined in this case
+    //   console.log("No such document!");
+    // }
+
+    }fetchData()
+  },[]);
+
+
+  //==============================================================================//
+
+  async function completeStreak(){
+    const today = new Date();
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate()-1)
+    const docRef = doc(db, "my-info", "1R01JaSkN66l356PKmnM");
+    const docSnap = await getDoc(docRef);
+    const docData = docSnap.data()
+
+    if (docData.is_completed == true){
+      await updateDoc(docRef, {
+        is_completed: false,
+        last_completed_day: yesterday,
+        streak: docData.streak-1
+      });
+      setStreak({
+        current_day: today.toLocaleDateString("en-US"),
+        is_completed: false,
+        name: docData.name,
+        start_date: docData.start_date,
+        last_completed_day: yesterday.toLocaleDateString("en-US"),
+        streak : docData.streak-1
+      });
+      
+    }
+    else if(docData.is_completed==false){
+      await updateDoc(docRef, {
+        is_completed: true,
+        last_completed_day:today,
+        streak: docData.streak +1
+      });
+      setStreak({
+        current_day: today.toLocaleDateString("en-US"),
+        is_completed: true,
+        name: docData.name,
+        start_date: docData.start_date,
+        last_completed_day: today.toLocaleDateString("en-US"),
+        streak : docData.streak+1
+      });
+
+    }
+    
+    
+    
+    
+  }
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div>
+        <h1>{streak.name}</h1>
+        <h1>Streak: {streak.streak}</h1>
+        <h3>Is Completed: {streak.is_completed ?(<p>{`${streak.is_completed}`}</p>):(<p>{`${streak.is_completed}`}</p>)}</h3>
+
+        <h3>{streak.start_date ? (
+      <p>Start Date: {streak.start_date.toDate().toLocaleDateString("en-US")}</p>
+    ) : (
+      <p>Loading...</p>
+    )}</h3>
+
+    <h3>Today: {String(streak.current_day)}</h3>
+    <h3>Last Completed: {String(streak.last_completed_day)}</h3>
+    
+    <button onClick={completeStreak}>Yes!</button>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        
+      
     </main>
   )
 }
