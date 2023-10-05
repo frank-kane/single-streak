8825a0947e41a0b8b3d1ae7a948d8dcbf1b74754
Kandy-Kane <frankmyster@gmail.com>
2023-10-04 23:43:35 -0400

    got log out working sort of

--- components/navbar.js
+++ components/navbar.js
@@ -11,11 +11,34 @@ import 'reactjs-popup/dist/index.css';
 import { useRouter } from 'next/router';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { getAuth, signOut } from "firebase/auth";
+
 
 
 export default function NavBar(props){
 
+  const [showUserInfo, setShowUserInfo] = useState(false)
+
   const router = useRouter();
+
+  const handleLogOut = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      router.push({
+        pathname: '/',
+      })
+      // Sign-out successful.
+    }).catch((error) => {
+      console.error("Error during logout:", error);
+      // An error happened.
+    });
+
+
+    
+
+  }
+
+  
 
     return(
         <div className='nav-bar'>
@@ -28,7 +51,15 @@ export default function NavBar(props){
           <h3>Party</h3>
           <h3>About</h3>  
           <h3>Help</h3>
-          <img src="user-account-icon.png" alt="" className='user-account-icon' />
+          <img src="user-account-icon.png" alt="" className='user-account-icon' onClick={()=> setShowUserInfo(true)} />
+          {showUserInfo &&
+          <div className='user-info-modal-overlay'>
+          <div className='user-info'>
+            <button onClick={handleLogOut}>Log Out</button>
+            <button onClick={()=>setShowUserInfo(false)}>Cancel</button>
+          </div>
+          </div>
+          }
 
         </div>
         
