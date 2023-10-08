1bd89232d0faa0fe03f242207e9b301702e857e1
Kandy-Kane <frankmyster@gmail.com>
2023-10-08 00:55:16 -0400

    Started got local storage set up

--- pages/index.js
+++ pages/index.js
@@ -13,75 +13,76 @@ import 'react-tabs/style/react-tabs.css';
 import NavBar from "../components/navbar"
 import {db} from '../components/firebase-config'
 import { Timestamp, doc, getDoc, getDocs,updateDoc, collection,query,where,addDoc  } from "firebase/firestore";
+// import UnityGame from "./unity-game"
+
 
 export default function Home(){
 
   const [user, setUser] = useState({});
   const collectionRef = collection(db, 'my-info');
-  const router = useRouter();
+  // const router = useRouter();
 
 
-  useEffect(()  =>{
-    const fetchData = async()=>{
-      const query = query(collectionRef, where("user_info.email", "==", "frankmyster@gmail.com"));
-      const querySnapshot = await getDocs(query);
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collectionRef, where("login.email", "==", "frankmyster2@gmail.com"));
+      const querySnapshot = await getDocs(q);
+  
       querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-      docID = String(doc.id);
-      
-      setUser({
-        docID: String(doc.id),
-      })
-      
-      
-    });
-      const docRef = doc(db, "my-info", user.docID);
-      const docSnap = await getDoc(docRef);
-      const docData = docSnap.data()
-      setUser({
-        login:{
-          email:docData.email,
-          username:docData.username,
-          password:docData.password,
-      },
-      user_info:{
-        gender:docData.gender,
-        bfp:docData.bfp,
-        height:docData.height,
-        weight:docData.weight,
-
-
-      },
-      stats:{
-          lvl:docData.lvl,
-          exp:docData.exp,
-          vig:docData.vig,
-          str:docData.str,
-          int:docData.int,
-          dex:docData.dex,
-          luck:docData.luck,
-          scale_factor:docData.scale_factor,
+        const docData = doc.data();
   
-      },
-      
-      habits:docData.habits,
+        setUser((prevUser) => ({
+          ...prevUser,
+          docID: String(doc.id),
+          login: {
+            email: docData.login.email,
+            username: docData.login.username,
+            password: docData.login.password,
+          },
+          user_info: {
+            gender: docData.user_info.gender,
+            bfp: docData.user_info.bfp,
+            height: docData.user_info.height,
+            weight: docData.user_info.weight,
+          },
+          stats: {
+            lvl: docData.stats.lvl,
+            exp: docData.stats.exp,
+            str: docData.stats.str,
+            int: docData.stats.int,
+            dex: docData.stats.dex,
+          },
+          habits: docData.habits,
+        }));
+      });
   
-      
-      
-      })
+      console.log("Signed In User: ", JSON.stringify(user)); // This might not log as expected due to asynchronous behavior
+      localStorage.setItem('user', JSON.stringify(user)); // Convert user object to JSON before saving to localStorage
+    };
   
-    console.log("Signed In User: "+user)
-    localStorage.setItem('user', user);
-    }
-
     fetchData();
-    
-
-  },[]);
+  }, []);
   
 //=======================Get Doc Information==========================================//
+useEffect(() => {
+  // Check if user has data to store
+  if (Object.keys(user).length > 0) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+}, [user]);
+
+useEffect(() => {
+  // Retrieve user data from localStorage
+  const userData = localStorage.getItem('user');
+
+  if (userData) {
+    // Parse the stored JSON data back into an object
+    const parsedUserData = JSON.parse(userData);
     
+    // Set the user state with the retrieved data
+    setUser(parsedUserData);
+  }
+}, []);
     
 
 
@@ -101,11 +102,31 @@ export default function Home(){
 
   return (
     <div className='index-container'>
-      <NavBar/>
-      <center>
-        
-      </center>
-    </div>
+    <NavBar />
+    <center>
+      {/* <UnityGame/> */}
+
+      <h1>User Information</h1>
+      {user && Object.keys(user).length > 0 ? (
+        <div>
+          <p>Email: {user.login.email}</p>
+          <p>Username: {user.login.username}</p>
+          <p>Gender: {user.user_info.gender}</p>
+          <p>BFP: {user.user_info.bfp}</p>
+          <p>Height: {user.user_info.height}</p>
+          <p>Weight: {user.user_info.weight}</p>
+          <p>Level: {user.stats.lvl}</p>
+          <p>Experience: {user.stats.exp}</p>
+          <p>Strength: {user.stats.str}</p>
+          <p>Intelligence: {user.stats.int}</p>
+          <p>Dexterity: {user.stats.dex}</p>
+          <p>Habits: {JSON.stringify(user.habits)}</p>
+        </div>
+      ) : (
+        <p>No user data found in localStorage.</p>
+      )}
+    </center>
+  </div>
     
   );
 
