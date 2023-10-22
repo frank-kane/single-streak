fb57f6e7d3b17c7b13e6a9697a204e07ca6b9dce
Kandy-Kane <frankmyster@gmail.com>
2023-10-22 19:38:02 -0400

    alot

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,6 @@ import { auth } from '../components/firebase-config';
 import { NavLink, Router, useNavigate } from 'react-router-dom'
 import Link from 'next/link'
 import firebase from 'firebase/app';
-import axios from 'axios';
-import cheerio from 'cheerio';
-// import puppeteer from 'puppeteer';
-
 import 'firebase/auth';
 import { useRouter } from 'next/router';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
@@ -22,11 +18,19 @@ import { arrayUnion, arrayRemove  } from "firebase/firestore";
 // import MyTabs from "../components/tabs"
 // import Habits from "../components/habits"
 import { onSnapshot } from "firebase/firestore";
+import axios from 'axios';
+import cheerio from 'cheerio';
+import AnimeList from './anime-list.json'; // Adjust the path to match your file structure
+import AnimeList2 from './anime-list'
+import animeData from './anime-list.json';
 
 
 export default function Home(){
+  const [myAnimeTitles, setMyAnimeTitles] = useState([]);
+  const [userInfo, setUserInfo] = useState({});
   const [stats, setStats] = useState({});
   const [habits, setHabits] = useState([]);
+  const [weapons, setWeapons] = useState([]);
   const [items, setItems] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [newHabitData, setNewHabitData] = useState({
@@ -36,12 +40,18 @@ export default function Home(){
   const usersCollection = collection(db, 'users'); // Reference to the "users" collection
   const userDocRef = doc(usersCollection, '8yciXAQXy9GTxmuclEX6'); // Reference to the specific user document
   const habitsRef = collection(userDocRef, 'habits');
+  const weaponsRef = collection(userDocRef, 'weapons');
   const itemsRef = collection(userDocRef, 'items');
+  const myAnimeRef = collection(userDocRef, 'anime');
   const today = new Date(); // Current date and time
   const yesterday = new Date(today);
   yesterday.setDate(today.getDate() - 1);
   const twoDaysAgo = new Date(today);
   twoDaysAgo.setDate(today.getDate() - 2);
+
+
+  
+
 
   const openOrCloseModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -55,10 +65,22 @@ export default function Home(){
       [name]: value,
     });
   };
+
+  
   
 
  
-
+  React.useEffect(() => {
+    const unsubscribe = onSnapshot(myAnimeRef, function(snapshot) {
+        // Sync up our local notes array with the snapshot data
+        const animeArr = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+        }))
+        setMyAnimeTitles(animeArr)
+    })
+    return unsubscribe
+  }, []);
 
 
   React.useEffect(() => {
@@ -105,6 +127,18 @@ export default function Home(){
 }, []);
 
 React.useEffect(() => {
+  const unsubscribe = onSnapshot(weaponsRef, function(snapshot) {
+      // Sync up our local notes array with the snapshot data
+      const weaponsArr = snapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id
+      }))
+      setWeapons(weaponsArr)
+  })
+  return unsubscribe
+}, []);
+
+React.useEffect(() => {
   const unsubscribe = onSnapshot(itemsRef, function(snapshot) {
       // Sync up our local notes array with the snapshot data
       const itemsArr = snapshot.docs.map(doc => ({
@@ -136,6 +170,10 @@ React.useEffect(() => {
       const userData = docSnapshot.data();
       // Assuming you have a 'stats' field in your user document
       const userStats = userData.stats;
+      setUserInfo({
+        username: userData.username,
+        money: userData.money
+      })
       setStats(userStats);
       console.log(stats)
     }
@@ -154,14 +192,6 @@ React.useEffect(() => {
 
 
 async function createNewHabit() {
-  // const newHabit = {
-  //     name: "Your Habit",
-  //     is_completed: false,
-  //     last_completed: "",
-  //     streak: 0,
-  //     type: "physical"
-  // }
-  // const newHabitRef = await addDoc(habitsRef, newHabit)
 
   const newHabit = { ...newHabitData, is_completed: false, last_completed: new Date(), streak: 0 };
   await addDoc(habitsRef, newHabit);
@@ -174,6 +204,25 @@ async function createNewHabit() {
 async function deleteHabit(noteId) {
   const habitsRef = collection(userDocRef, 'habits');
   const docRef = doc(habitsRef, noteId)
+  await deleteDoc(docRef)
+}
+
+async function addAnime(animeIndex) {
+  const animeToAdd = animeData.titles[animeIndex]; // Get the anime object from animeData
+
+  try {
+    // Add the anime object to your myAnimeRef collection in Firebase
+    await addDoc(myAnimeRef, animeToAdd);
+    // You may want to show a success message or update your local state here.
+  } catch (error) {
+    console.error("Error adding anime:", error);
+    // Handle the error (e.g., show an error message).
+  }
+}
+
+async function deleteAnime(animeId) {
+  
+  const docRef = doc(myAnimeRef, animeId)
   await deleteDoc(docRef)
 }
 
@@ -252,19 +301,39 @@ async function completeHabit(noteId) {
 
 return (
   <div className='index-container'>
-    <h1>User</h1>
+    <div className='user-info'>
+      <h1>User</h1>
+      <h4>{userInfo.username||""}</h4>
+      <h4>${userInfo.money||0}</h4>
+      <h4>Lvl: {stats.level||0}</h4>
+      <h4>exp: {stats.current_exp||0}/{stats.next_exp}</h4>
+    </div>
+    
+
+
     {stats && (
-      <div>
-        <h4>Lvl: {stats.level||0}</h4>
-        <h4>exp: {stats.current_exp||0}/{stats.next_exp}</h4>
-        <h4>str: {stats.strength||0}</h4>
-        <h4>dex: {stats.dexerity||0}</h4>
-        <h4>int: {stats.intellect||0}</h4>
-      </div>
+        <div className='main-stats'>
+          <h1>Stats</h1>
+          <div>
+            <div>
+              <div><h2>str: {stats.strength||0}</h2></div>
+              <div><h2>dex: {stats.dexerity||0}</h2></div>
+            </div>
+            <div>
+              <div><h2>int: {stats.intellect||0}</h2></div>
+              <div><h2>chr: {stats.charisma||0}</h2></div>
+            </div>
+            <div>
+              <div><h2>con: {stats.constitution||0}</h2></div>
+              <div><h2>wis: {stats.wisdom||0}</h2></div>
+            </div>
+          </div>
+        </div>
     )}
     <div className='habits-container'>
     <h1>Habits</h1>
-    <button onClick={openOrCloseModal}>Add Habit</button>
+    {habits.length <7 &&<button onClick={openOrCloseModal}>Add Habit</button>}
+    
     {isModalOpen && (
   <div className="modal">
     <div className="modal-content">
@@ -294,12 +363,12 @@ return (
         {habits.length > 0 ? habits.map((habit) => (
           
           <div key={habit.id} className='habit'>
-            <table >
-              <tr>{habit.name}</tr>
-              <tr>{habit.streak}</tr>
-              <tr>{habit.is_completed == false && habit.last_completed <= yesterday?<img onClick={() => completeHabit(habit.id)} className='icon' src="frozen-flame.png" alt="" />:<img onClick={() => completeHabit(habit.id)} className='icon' src="fire.gif" alt="" />}</tr>
-              <tr><button onClick={() => deleteHabit(habit.id)}>Delete</button></tr>
-            </table>
+            <div >
+              <div>{habit.name}</div>
+              <div>{habit.streak}</div>
+              <div>{habit.is_completed == false && habit.last_completed <= yesterday?<img onClick={() => completeHabit(habit.id)} className='icon' src="frozen-flame.png" alt="" />:<img onClick={() => completeHabit(habit.id)} className='icon' src="fire.gif" alt="" />}</div>
+              <div><button onClick={() => deleteHabit(habit.id)}>Delete</button></div>
+            </div>
           </div>
           )) : <div>
           <h1>No Habits</h1>
@@ -315,32 +384,98 @@ return (
     
     <div className='items-container'> 
     <h1>Items</h1>
+    <Tabs>
+    <TabList>
+      <Tab>Weapons</Tab>
+      <Tab>Healing Items</Tab>
+    </TabList>
 
-    <div className='all-items'>
-        {items.length > 0 ? items.map((item) => (
+    <TabPanel>
+    <div className='all-weapons'>
+        {weapons.length > 0 ? weapons.map((weapon) => (
+          <div className='weapon' key={weapon.id}>
+            <div>
+              <img className='item' src={`${weapon.name}.png`}/>
+            </div>
+          <div key={weapon.id}>
+            {weapon.name}
+          </div>
+          <div>Dmg:
+            {weapon.damage}
+          </div>
+      </div>
+        )) : <h1>No Weapons</h1>}
+    </div>
+    </TabPanel>
+
+    <TabPanel>
+    {items.length > 0 ? items.map((item) => (
+          <div className='item-container' key={item.id}>
           <div>
-          <ul>
-            <li>
+            <div>
               <img className='item' src={`${item.name}.png`}/>
-            </li>
-          <li key={item.id}>
+            </div>
+          <div key={item.id}>
             {item.name}
-          </li>
-          <li>Dmg:
-            {item.damage}
-          </li>
-      </ul>
+          </div>
+          <div>Heal:
+            {item.heal}
+          </div>
+      </div>
       </div>
         )) : <h1>No Items</h1>}
-    </div>
+    </TabPanel>
+  </Tabs>
+    
 
     </div>
 
-    <h1>Anime</h1>
+    <div className='anime-container'>
+    <h1>My Anime</h1>
+    <div className='all-my-anime'>
+        {myAnimeTitles.length > 0 ? myAnimeTitles.map((anime) => (
+          
+          <div key={anime.id} className='habit'>
+            <div >
+                <div key={anime.id}>{anime.name}</div>
+                <img src={anime.img} key={anime.id} />
+                <button onClick={()=>deleteAnime(anime.id)}>-</button>
+            
+            </div>
+          </div>
+          )) : <div>
+          <h1>No Habits</h1>
+
+        </div> 
+        }
+      </div>
+
+    </div>
+
+      <div className='anime-site-container'>
+        <h1>Site Anime</h1>
+        <div className='anime-list'>
+      <div>
+        {animeData.titles.map((anime, index) => (
+            <div>
+                <div key={index}>{anime.name}</div>
+                <img src={anime.img} key={index} />
+                <button onClick={()=>addAnime(index)}>+</button>
+
+            </div>
+          
+
+        ))}
+      </div>
+
+      
+    </div>
+        
+      </div>
 
     
 
-    <h1>Site Anime</h1>
+    
 
     
 
